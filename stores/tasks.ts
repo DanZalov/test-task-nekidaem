173a80e068583c12da4b9f047684840c6af4fd43
DanZalov <danzalov@gmail.com>
2024-03-20 00:03:56 +0400

    remove redundant data, packages

--- stores/tasks.ts
+++ stores/tasks.ts
@@ -26,84 +26,14 @@ export interface TasksItem {
 
 const baseURL = 'https://trello.backend.tests.nekidaem.ru/api/v1/'
 
-const defaultList1: ListItem[] = [
-  {
-    value: 'vue.draggable',
-    id: 0,
-  },
-  {
-    value: 'draggable',
-    id: 1,
-  },
-  {
-    value: 'component',
-    id: 2,
-  },
-  {
-    value: 'for',
-    id: 3,
-  },
-  {
-    value: 'vue.js 2.0',
-    id: 4,
-  },
-  {
-    value: 'based',
-    id: 5,
-  },
-  {
-    value: 'on',
-    id: 6,
-  },
-  {
-    value: 'Sortablejs',
-    id: 7,
-  },
-]
-
-const defaultList2: ListItem[] = [
-  {
-    value: 'vue.draggable.next',
-    id: 10,
-  },
-  {
-    value: 'draggable',
-    id: 11,
-  },
-  {
-    value: 'component',
-    id: 12,
-  },
-  {
-    value: 'for',
-    id: 13,
-  },
-  {
-    value: 'next 3.0',
-    id: 14,
-  },
-  {
-    value: 'based',
-    id: 15,
-  },
-  {
-    value: 'on',
-    id: 16,
-  },
-  {
-    value: 'Sortablejs',
-    id: 17,
-  },
-]
-
 export const useTasksStore = defineStore('tasks', {
   state: () => ({
     login: 'DanilApiTest',
     password: 'ComplexPass',
-    hold: defaultList1,
-    progress: defaultList2,
-    review: defaultList1,
-    approved: defaultList2,
+    hold: [] as ListItem[],
+    progress: [] as ListItem[],
+    review: [] as ListItem[],
+    approved: [] as ListItem[],
     accessToken: '',
     refreshToken: '',
   }),
@@ -334,5 +264,4 @@ export const useTasksStore = defineStore('tasks', {
       }
     },
   },
-  persist: true,
 })
